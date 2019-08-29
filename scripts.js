cf7f39feb303eb5d8d9ff70e18f03e7a3bd61de4
David Diaz <daemdisa@aol.com>
2019-08-29 12:00:43 -0500

    done

---
+++ scripts.js
@@ -0,0 +1,34 @@
+$(document).ready( readyNow );
+
+function readyNow (){
+    console.log( 'hello from jquery');
+    $( '#pizzaPrices' ).remove();
+
+    $('ul').children().addClass( 'bold' );
+
+    $( '#pizzeriaName' ).text()
+
+    let val = $( '#pizzeriaName' ).text();
+    console.log( 'this is val:', val);
+
+    $( '#pizzeriaName' ).text( `Davi's Place` );
+
+    $( '#orderBtn' ).on('click', function(){
+        handleClick();
+    })
+    $( 'header' ).on('click', '#secretBtn', function(){
+        secretFunction();
+    })
+}
+
+function handleClick(){
+    console.log('button click');
+    $('#orderBtn').toggleClass('updateOrderBtn');
+    $('header').append(`<button id="secretBtn">secret button</button>`)
+}
+
+function secretFunction(){
+    alert('shhhhhhhh hhh sh');
+    $( '#secretBtn').remove();
+}
+
